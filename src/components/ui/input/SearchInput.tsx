04999f2eacd58b2e0a6bc338d8f497215dae51f1
mynolog <minholee.log@gmail.com>
2025-03-21 20:47:01 +0900

    fix: SearchInput 컴포넌트 타입 에러 수정

--- src/components/ui/input/SearchInput.tsx
+++ src/components/ui/input/SearchInput.tsx
@@ -3,17 +3,20 @@
 import type { BaseInputProps } from './BaseInput'
 import BaseInput from './BaseInput'
 
-interface SearchInputProps extends Omit<BaseInputProps, 'type'> {}
+interface SearchInputProps extends Omit<BaseInputProps, 'type'> {
+  type?: 'text'
+}
 
 export default function SearchInput({
   value,
   onChange,
   placeholder = '여기에서 이미지 검색이 가능합니다!',
   className = '',
+  type = 'text',
 }: SearchInputProps) {
   return (
     <BaseInput
-      type="text"
+      type={type}
       value={value}
       onChange={onChange}
       placeholder={placeholder}
